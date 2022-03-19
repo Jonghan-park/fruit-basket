b7fcfa206efd86eb8a42c0b7ce154e656d409393
jonghan-park <jonghan.park@edu.sait.ca>
2022-03-18 21:58:23 -0600

    Update fruits

---
+++ src/fruits.js
@@ -0,0 +1,24 @@
+export default[
+    {
+        id: 1,
+        name: 'Apple',
+        quantities: 2,
+        image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.qEvRk2ip9vubQ-_xnfTj1AHaFj%26pid%3DApi&f=1'
+    },{
+        id:2,
+        name: 'Banana',
+        quantities: 5,
+        image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.dayfreshkuwait.com%2Fpub%2Fmedia%2Fcatalog%2Fproduct%2Fcache%2F857e541ec9f008db3ccadf7b2010b71e%2Fb%2Fa%2Fbanana-philippines1.jpg&f=1&nofb=1'
+    },{
+        id:3,
+        name:'Watermelon',
+        quantities: 4,
+        image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.oU8AIoRAZj9CeoL_qeaVcwHaFS%26pid%3DApi&f=1'
+    },{
+        id:4,
+        name:'Orange',
+        quantities: 5,
+        image:'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmealstudio.com%2Fwp-content%2Fuploads%2F2019%2F10%2FOrange-1.jpg&f=1&nofb=1'
+    }
+
+]
