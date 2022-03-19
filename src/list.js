8192425579a5f342c3f7208d895449c3342e784d
jonghan-park <jonghan.park@edu.sait.ca>
2022-03-19 08:45:03 -0600

    Update lis.js

--- src/list.js
+++ src/list.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
-export const list = () => {
+export const list = ({fruits}) => {
   return (
-    <div>list</div>
-  )
-}
+    <>
+    {fruits.map((fruit)=>{
+        const {id,name,quantities,image} = fruit;
+        return <article key={id} className={fruit}> 
+        <img src={image}></img>
+        <div>
+            <h4>{name}</h4>
+            <p>{quantities} boxes</p>
+        </div>
+        </article>
+    })}
+    </>
+  );
+};
 
