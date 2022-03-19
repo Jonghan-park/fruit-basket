c622b317ebecca5c838d7c3a170e0616447a4561
jonghan-park <jonghan.park@edu.sait.ca>
2022-03-19 08:33:55 -0600

    Update fruits

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-
+import React, { useState } from 'react';
 import './App.css';
+import list from './list.js';
+import data from './data.js';
 
 function App() {
+    const [fruits, setFruits] = useState(data);
 
+    <main>
+        <section className='container'>
+            <list />
+        </section>
+    </main>
 }
 
 export default App;
